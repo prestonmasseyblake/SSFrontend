ba9d13f635ea391c125c6fbf0c27c926297af707
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2022-11-18 22:52:18 -0800

    first commit

---
+++ src/components/snowBackground.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./snowBackground.scss";
+function snowBackground({children}) {
+    return (
+      <div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+        <div className="snowflake"></div>
+            <div className="snowflake"></div>
+            { children }
+      </div>
+    );
+}
+
+export default snowBackground
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SnowBackground from '../components/snowBackground'
+import SwipeContainer from '../components/SwipeContainer'
+
+function Home() {
+    return (
+        <div>
+            <SnowBackground />
+            <SwipeContainer/>
+        </div>
+    )
+}
+
+export default Home
